ff62d39e9832b3ddf55e6aadec165e60e0947362
Brian K. Dew <brian.k.dew@gmail.com>
2026-03-18 16:05:52 -0400

    Pin Node 20 and align Expo test tooling

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,18 +1,8 @@
 module.exports = {
-  testEnvironment: 'node',
+  preset: 'jest-expo/node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
   testPathIgnorePatterns: ['/node_modules/', '/archive/'],
   modulePathIgnorePatterns: ['<rootDir>/archive/'],
-  transform: {
-    '^.+\\.(ts|tsx)$': [
-      'ts-jest',
-      {
-        tsconfig: '<rootDir>/tsconfig.json',
-      },
-    ],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!src/**/*.d.ts',
